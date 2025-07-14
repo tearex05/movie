c71182154ceaa0a008343248e082f4fce52b5b6c
unknown <rezyek05@gmail.com>
2025-07-14 10:19:38 +0330

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./css/index.css";
+import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter basename="/movie/">
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </HashRouter>
+  </React.StrictMode>
 );
